d41eefbb367504ac166d1e13e18a8fdcd985eb14
Harsh Dixit <harshdixit573@gmail.com>
2024-08-18 17:09:33 +0530

    transfer frontend added

--- apps/user/app/transfer/page.tsx
+++ apps/user/app/transfer/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import prisma from "@repo/db/client";
+import { AddMoney } from "../../components/AddMoney";
+import { BalanceCard } from "../../components/BalanceCard";
+import { OnRampTransactions } from "../../components/OnRampTransaction";
+import { getServerSession } from "next-auth";
+import  {options}  from "@repo/auth/options";
 
-const page = () => {
-  return (
-    <div>transfer</div>
-  )
+async function getBalance() {
+    const session = await getServerSession(options);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
 }
 
-export default page
+async function getOnRampTransactions() {
+    const session = await getServerSession(options);
+    const txns = await prisma.onRampTransaction.findMany({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+}
+
+export default async function() {
+    const balance = await getBalance();
+    const transactions = await getOnRampTransactions();
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Transfer
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <AddMoney />
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked} />
+                <div className="pt-4">
+                    <OnRampTransactions transactions={transactions} />
+                </div>
+            </div>
+        </div>
+    </div>
+}
